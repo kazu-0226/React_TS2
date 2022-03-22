b308db9eef15d7130b250117ae611297bd9bd347
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-03-22 14:30:31 +0900

    コンポーネントの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 import { Todos } from "./Todos";
 import "./styles.css";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -21,9 +16,11 @@ export default function App() {
   };
   return (
     <div className="App">
+      <Text color="red" fontSize="18px"></Text>
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todos
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
